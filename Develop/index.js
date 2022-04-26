b3413b047cb94dd34ed00b9363c5baadd25c51ed
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-04-26 05:24:36 -0500

    added array of questions variable to the index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,9 @@
 // TODO: Include packages needed for this application
+const generatePage = require('./utils/generateMarkdown.js');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of this project?', '', 'Provide a description of the project.', 'What are the steps required to install this project?', 'How do you use this projects', 'Pick a License.', 'Were there any contributors?', 'Test', 'Do You have any questions for me/ us'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +13,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
